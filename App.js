34fabe975cba92c8e83add9dc9faabaffd770ee8
ndessai <ndessai@gmail.com>
2019-09-01 14:36:19 -0400

    code cleanup

--- App.js
+++ App.js
@@ -1,43 +1,44 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, Button } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import { Header, LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
+import {View, Text, Button} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
 class HomeScreen extends React.Component {
-    render() {
-      return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Home Screen</Text>
-          <Button
+  render() {
+    return (
+      // eslint-disable-next-line react-native/no-inline-styles
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Home Screen</Text>
+        <Button
           title="Go to Details"
           onPress={() => this.props.navigation.navigate('Details')}
         />
-        </View>
-      );
-    }
+      </View>
+    );
   }
-  
-  class DetailsScreen extends React.Component {
-    render() {
-      return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Details Screen</Text>
-        </View>
-      );
-    }
-  }
+}
 
-  const AppNavigator = createStackNavigator(
-    {
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      // eslint-disable-next-line react-native/no-inline-styles
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+const AppNavigator = createStackNavigator(
+  {
     Home: HomeScreen,
     Details: DetailsScreen,
-    },
-    {
-      initialRouteName: 'Home',
-    }
-  );
-  
+  },
+  {
+    initialRouteName: 'Home',
+  },
+);
+
 const AppContainer = createAppContainer(AppNavigator);
 
 export default class App extends React.Component {
