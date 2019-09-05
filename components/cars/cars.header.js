440b1d0d2011081c1ae0ee159b9c6b69c46547f4
ndessai <ndessai@gmail.com>
2019-09-05 00:51:11 -0400

    Cleaned Header - const styles

--- components/cars/cars.header.js
+++ components/cars/cars.header.js
@@ -1,45 +1,93 @@
 import React from 'react';
 import {View, Text, ImageBackground, TouchableOpacity} from 'react-native';
-
 import CustomIcon from '../custom.icons';
+
+const HeaderStyles = {
+  Height: {height: 100},
+  IconColor: '#ffffff',
+  ImageBackground: require('../../assets/car_header_bg.png'),
+  ImageSize: {width: '100%', height: '100%'},
+  Container: {marginTop: 55, flex: 1, height: 36, flexDirection: 'row'},
+  BackContainer: {width: 36, paddingLeft: 13, paddingTop: 9},
+  BackIconSize: {Width: 10, Height: 18},
+  SearchParamsContainer: {
+    height: 36,
+    flexGrow: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  SearchCityContainer: {flex: 1, height: 19, flexDirection: 'row'},
+  SearchCityText: {fontSize: 16, color: '#ffffff'},
+  EditIconSize: {Width: 18, Height: 16},
+  EditIcon: {marginLeft: 3},
+  DatesContainer: {flex: 1, height: 14},
+  DatesText: {fontSize: 12, color: '#ffffff'},
+  HamburgerIconSize: {Width: 20, Height: 18},
+  HamburgerIconContainer: {width: 36},
+  HamburgerIcon: {margin: 8},
+};
 
 export class CarsHeader extends React.Component {
   render() {
     return (
-      <View style={{height: 100}}>
+      <View style={HeaderStyles.Height}>
         <ImageBackground
-          source={require('../../assets/car_header_bg.png')}
-          // eslint-disable-next-line react-native/no-inline-styles
-          style={{width: '100%', height: '100%'}}>
-          <View style={{marginTop: 55, flex: 1, height: 36, flexDirection: 'row'}}>
-            <View style={{width: 36, paddingLeft: 13, paddingTop: 9}}>
-            <TouchableOpacity onPress={()=>{}} >
+          source={HeaderStyles.ImageBackground}
+          style={HeaderStyles.ImageSize}>
+          <View style={HeaderStyles.Container}>
+            <View style={HeaderStyles.BackContainer}>
+              <TouchableOpacity onPress={() => {}}>
                 <View>
-                <CustomIcon name='uniE75B' size={10,18} color="#ffffff"/>
+                  <CustomIcon
+                    name="uniE75B"
+                    size={
+                      (HeaderStyles.BackIconSize.Width,
+                      HeaderStyles.BackIconSize.Height)
+                    }
+                    color={HeaderStyles.IconColor}
+                  />
                 </View>
-            </TouchableOpacity>
+              </TouchableOpacity>
             </View>
-            <View style={{height: 36, flexGrow: 1, flexDirection: 'column', alignItems: 'center'}}>
-              <View style={{flex: 1, height: 19, flexDirection: 'row'}}>
-                <Text style={{fontSize: 16, color: '#ffffff'}}>NYC - MCO</Text>
-                <TouchableOpacity onPress={()=>{}} style={{marginLeft: 3}} >
-                    <View>
-                    <CustomIcon name='uniE693' size={18,16} color="#ffffff"/>
-                    </View>
+            <View style={HeaderStyles.SearchParamsContainer}>
+              <View style={HeaderStyles.SearchCityContainer}>
+                <Text style={HeaderStyles.SearchCityText}>NYC - MCO</Text>
+                <TouchableOpacity
+                  onPress={() => {}}
+                  style={HeaderStyles.EditIcon}>
+                  <View>
+                    <CustomIcon
+                      name="uniE693"
+                      size={
+                        (HeaderStyles.EditIconSize.Width,
+                        HeaderStyles.EditIconSize.Height)
+                      }
+                      color={HeaderStyles.IconColor}
+                    />
+                  </View>
                 </TouchableOpacity>
               </View>
-              <View style={{flex: 1, height: 14}}>
-                <Text style={{fontSize: 12, color: '#ffffff'}}>Sep 12, 03:31pm - Sep 15, 06:50pm</Text>
+              <View style={HeaderStyles.DatesContainer}>
+                <Text style={HeaderStyles.DatesText}>
+                  Sep 12, 03:31pm - Sep 15, 06:50pm
+                </Text>
               </View>
             </View>
-            <View style={{width: 36}}>
-              <View style={{margin: 8, justifyContent: 'space-between', flexDirection: 'column'}}>
-              <TouchableOpacity onPress={()=>{}} >
-                    <View>
-                    <CustomIcon name='uniE648' size={20, 18} color="#ffffff"/>
-                    </View>
-                </TouchableOpacity>
-              </View>
+            <View style={HeaderStyles.HamburgerIconContainer}>
+              <TouchableOpacity
+                style={HeaderStyles.HamburgerIcon}
+                onPress={() => {}}>
+                <View>
+                  <CustomIcon
+                    name="uniE648"
+                    size={
+                      (HeaderStyles.HamburgerIconSize.width,
+                      HeaderStyles.HamburgerIconSize.Height)
+                    }
+                    color={HeaderStyles.IconColor}
+                  />
+                </View>
+              </TouchableOpacity>
             </View>
           </View>
         </ImageBackground>
