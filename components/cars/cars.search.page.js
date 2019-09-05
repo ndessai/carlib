e7d1b7dbb58d03476a7ec963206e9afdf510da8c
ndessai <ndessai@gmail.com>
2019-09-05 01:16:03 -0400

    event hooked examples

--- components/cars/cars.search.page.js
+++ components/cars/cars.search.page.js
@@ -4,11 +4,17 @@ import {CarsHeader} from './cars.header';
 import {CarsListingBody} from './listing/cars.listing.body';
 
 export class CarsSearchPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {ButtonName: 'None'};
+  }
+
   render() {
     return (
       <View style={{flex: 1, flexDirection: 'column'}}>
-        <CarsHeader backClicked={() => {}} editClicked={() => {}} menuClicked={() => {}} from ="NYC" to ="MCO" fromTime = "Sep 12 3:30pm"
+        <CarsHeader backClicked={() => {this.setState({ButtonName: 'Back'});}} editClicked={() => {this.setState({ButtonName: 'Edit'});}} menuClicked={() => {this.setState({ButtonName: 'Menu'});}} from ="NYC" to ="MCO" fromTime = "Sep 12 3:30pm"
         toTime = "Sep 15 5:12pm"/>
+        <Text>{this.state.ButtonName}</Text>
         <CarsListingBody />
       </View>
     );
